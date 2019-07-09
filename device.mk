1ea6c9861d3ecf8df4c782a6a10141ddaa95f48c
Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
2019-07-09 14:05:43 +0000

    Sanders: gps: import gps from LA.UM.7.6.r1-05300-89xx.0 tag
    Imports gps from CAF LA.UM.7.6.r1-05300-89xx.0 tag.
    
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -181,16 +181,14 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     libgnss \
-    libgnsspps \
-    android.hardware.gnss@1.0-impl-qti
+    liblocation_api \
+    libloc_core \
+    libgps.utils \
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
 
 # health
 PRODUCT_PACKAGES += \
